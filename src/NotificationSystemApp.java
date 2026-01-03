f5f33d1a1bab93c235c96182a2411709ba1ceadb
Arshpreet Singh <arshpreet9996@gmail.com>
2026-01-03 02:54:24 -0500

    Completed basic notificationApp.

--- src/NotificationSystemApp.java
+++ src/NotificationSystemApp.java
@@ -1,2 +1,39 @@
+import decorator.EncryptedMessage;
+import decorator.TimestampedMessage;
+import factory.NotificationFactory;
+import model.Message;
+import notification.Notification;
+import observer.AnalyticsObserver;
+import observer.LoggerObserver;
+import singleton.NotificationManager;
+import strategy.ImmediateSendStrategy;
+
 public class NotificationSystemApp {
+
+    public static void main(String[] args) {
+
+        //Base message
+
+        Message baseMessage = () -> "Hello user|";
+        Message encrypted = new EncryptedMessage(baseMessage);
+        Message decorated = new TimestampedMessage(encrypted);
+
+        // Create notification type
+
+        Notification notification
+                = NotificationFactory.createNotification("email");
+
+        notification.setStrategy(
+                new ImmediateSendStrategy()
+        );
+
+        // Singleton manager
+        NotificationManager notificationManager = NotificationManager.getInstance();
+        notificationManager.registerObserver(new LoggerObserver());
+        notificationManager.registerObserver(new AnalyticsObserver());
+
+        //send
+        notificationManager.sendNotification(notification, decorated);
+
+    }
 }
